d05dfb0e0c2c7d24363a4b10180d16459f9b0fd1
Devrimkutlu <69876232+Devrimkutlu@users.noreply.github.com>
2021-08-31 14:21:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import shutil
-src = r'C:\Users\z3r0\Desktop\copypasta\data\file1.xlsx'
-dst = r'C:\Users\z3r0\Desktop\copypasta\input\input.xlsx'
-shutil.copy(src, dst)
+def copypaste():
+    src = r'C:\Users\z3r0\Desktop\copypasta\data\file1.xlsx'
+    dst = r'C:\Users\z3r0\Desktop\copypasta\input\input.xlsx'
+    shutil.copy(src, dst)
+
+copypaste()
